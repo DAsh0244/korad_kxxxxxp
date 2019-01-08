a37a93a64bf344abad810b5c87cd3480c72f92f0
an-oreo <an.oreo@live.com>
2019-01-08 13:38:05 -0600

    started on basic unittests. moved to venv based env.

--- py/korad_api.py
+++ py/korad_api.py
@@ -8,9 +8,12 @@ Implemented as both:
 04 January-2019
 """
 
+import re
+import atexit
 import serial as _serial
 from numbers import Real as _Real
 from typing import Dict as _Dict, Union as _Union
+from time import sleep
 _ver_num = (0,0,1)
 __version__ = '.'.join(map(str,_ver_num))
 
@@ -36,14 +39,20 @@ KORAD_IREAD_CMD = lambda x: 'ISET{}?'.format(x).encode('utf-8')
 KORAD_VMEAS_CMD = lambda x: 'VOUT{}?'.format(x).encode('utf-8')
 KORAD_IMEAS_CMD = lambda x: 'IOUT{}?'.format(x).encode('utf-8')
 
+# korad model number regex
+# http://www.pyregex.com/?id=eyJyZWdleCI6Ii4qayg%2FUDxwYW5lbD5hfGQpKD9QPHZvbHRhZ2U%2BXFxkKSg%2FUDxjaGFubmVscz5cXGQpKD9QPGN1cnJlbnQ%2BXFxkKylwLioiLCJmbGFncyI6MiwibWF0Y2hfdHlwZSI6Im1hdGNoIiwidGVzdF9zdHJpbmciOiJSTkQgMzIwLUtEMzAwNVAgVjIuMCJ9
+KORAD_MODEL_REGEX = re.compile(r'.*k(?P<panel>a|d)(?P<max_voltage>\d)(?P<channels>\d)(?P<max_current>\d+)p.*',re.I)
+
 # OOP Interface
 
 class Korad:
     """
     Korad abstraction object
     """
-    def __init__(self,addr:str, timeout:float=KORAD_MAX_TIMEOUT):
+    def __init__(self, addr:str, timeout:float=KORAD_MAX_TIMEOUT, clamp:bool=True):
         self.addr = addr
+        self.clamp = clamp
+        self.timeout = timeout
         self._port = _serial.serial_for_url(addr,
                                            baudrate=KORAD_BAUD,
                                            parity=KORAD_PARITY,
@@ -51,13 +60,19 @@ class Korad:
                                            dsrdtr=KORAD_DATA_FLOW_CTRL,
                                            timeout=timeout
                                            )
-        self.id = self._identify()
+        self.id, self.channels, self.max_voltage, self.max_current = self._identify()
+        atexit.register(self.close)
+        # self.close = self._port.close
     
+    def send_cmd(self,cmd):
+        self._port.write(cmd)
+        sleep(self.timeout)
+
     @property
     def status(self)->_Dict[str, _Union[bool,str]]:
-        self._port.write(KORAD_STATUS_CMD)
+        self.send_cmd(KORAD_STATUS_CMD)
         status = ord(self._port.readline().rstrip().decode('utf-8'))
-        print('{0:08b}'.format(status))
+        # print('{0:08b}'.format(status))
         # parse status as described in notes:
         status_dict = {'output':bool(status&(1<<6)),
                        'mode':'CV' if status&1 else 'CC',
@@ -70,48 +85,80 @@ class Korad:
         return status_dict
     
     def _identify(self)->str:
-        self._port.write(KORAD_ID_CMD)
-        return self._port.readline().rstrip().decode('utf-8')
+        self.send_cmd(KORAD_ID_CMD)
+        id = self._port.readline().rstrip().decode('utf-8')
+        # info inferred from model number
+        model_info = KORAD_MODEL_REGEX.match(id).groupdict()
+        channels = 1 if model_info['channels'] == '0' else 3
+        max_current = int(model_info['max_current'])
+        max_voltage = int(model_info['max_voltage']+'0')
+        return id, channels, max_voltage, max_current
     
     def measured_voltage(self,ch=1):
-        self._port.write(KORAD_VMEAS_CMD(ch))
+        self.send_cmd(KORAD_VMEAS_CMD(ch))
         return float(self._port.readline().rstrip().decode('utf-8'))
     
     def measured_current(self,ch=1):
-        self._port.write(KORAD_IMEAS_CMD(ch))
+        self.send_cmd(KORAD_IMEAS_CMD(ch))
         return float(self._port.readline().rstrip().decode('utf-8'))
     
     def configured_voltage(self,ch=1):
-        self._port.write(KORAD_VREAD_CMD(ch))
+        self.send_cmd(KORAD_VREAD_CMD(ch))
         return float(self._port.readline().rstrip().decode('utf-8'))
     
     def configured_current(self,ch=1):
-        self._port.write(KORAD_IREAD_CMD(ch))
+        self.send_cmd(KORAD_IREAD_CMD(ch))
         return float(self._port.readline().rstrip().decode('utf-8'))
     
     def set_voltage(self,ch,voltage):
-        self._port.write(KORAD_VSET_CMD(ch,voltage))
+        if voltage < 0:
+            voltage = 0
+        elif voltage > self.max_voltage:
+            if self.clamp:
+                voltage = self.max_voltage
+            else:
+                raise ValueError('Voltage Too large: %s is greater than max voltage of %s' % voltage, self.max_voltage)
+        self.send_cmd(KORAD_VSET_CMD(ch,voltage))
     
     def set_current(self,ch,current):
-        self._port.write(KORAD_ISET_CMD(ch,current))
+        if current < 0:
+            current = 0
+        elif current > self.max_current:
+            if self.clamp:
+                current = self.max_current
+            else:
+                raise ValueError('Current Too large: %s is greater than max current of %s' % current, self.max_current)
+        self.send_cmd(KORAD_ISET_CMD(ch,current))
     
     def set_output(self,enable:bool):
         enable = 1 if enable else 0 
-        self._port.write(KORAD_OUTPUT_CMD(enable))
+        self.send_cmd(KORAD_OUTPUT_CMD(enable))
     
     def save_settings(self,loc:int):
-        self._port.write(KORAD_SAVE_CMD(loc))
+        self.send_cmd(KORAD_SAVE_CMD(loc))
     
     def recall_settings(self,loc:int):
-        self._port.write(KORAD_RECALL_CMD(loc))
+        self.send_cmd(KORAD_RECALL_CMD(loc))
     
     def set_ocp(self,enable:bool):
         enable = 1 if enable else 0
-        self._port.write(KORAD_OCP_CMD(enable))
+        self.send_cmd(KORAD_OCP_CMD(enable))
     
-    def __del__(self):
+    def close(self):
         self._port.close()
 
+    def configure(self,params:dict):
+        self.set_output(params.get('output',False))
+        self.set_ocp(params.get('ocp',False))
+        if params.get('voltage',False):
+            for entry in params['voltage']:
+                self.set_voltage(**entry)
+        if params.get('current',False):
+            for entry in params['current']:
+                self.set_current(**entry)
+
+    # def __del__(self):
+        # self.close()
 
 
 # Function Interface
@@ -121,7 +168,7 @@ def korad_set_voltage(addr:str,ch:int,voltage:_Real):
                         dsrdtr=KORAD_DATA_FLOW_CTRL,timeout=KORAD_MAX_TIMEOUT) as port:
         port.write(KORAD_VSET_CMD(ch,voltage))
 
-def korad_set_current(addr:str,ch:int,current:_Real):
+def korad_set_current(addr:str,ch:int,current:_Real, clamp):
     with _serial.Serial(addr,baudrate=KORAD_BAUD,parity=KORAD_PARITY,rtscts=KORAD_DATA_FLOW_CTRL,
                         dsrdtr=KORAD_DATA_FLOW_CTRL,timeout=KORAD_MAX_TIMEOUT) as port:
         port.write(KORAD_ISET_CMD(ch,current))
---
+++ py/korad_api_test.py
@@ -0,0 +1,63 @@
+import unittest
+from korad_api import *
+
+
+TEST_KORAD_INFO = {
+    'addr':'COM4',
+    'id':'RND 320-KD3005P V2.0',
+    'channels':1,
+    'max_voltage':30,
+    'max_current':5,
+}
+
+TEST_KORAD_CONFIG = {
+    'ocp':False,
+    'output':True,
+    'voltage':[{'ch':1,'voltage':3.29}],
+    'current':[{'ch':1,'current':0.01}],
+}
+
+class KoradObjectTests(unittest.TestCase):
+    def setUp(self):
+        self.psu = Korad(TEST_KORAD_INFO['addr'])
+        self.psu.configure(TEST_KORAD_CONFIG)
+
+    def tearDown(self):
+        self.psu.close()
+
+    def test_information(self):
+        korad_info = {
+            'addr':self.psu.addr,
+            'id':self.psu.id,
+            'channels':self.psu.channels,
+            'max_voltage':self.psu.max_voltage,
+            'max_current':self.psu.max_current,
+        }
+        self.assertEqual(korad_info, TEST_KORAD_INFO, 'ID Mismatch.')
+
+    def test_configured_voltage(self):
+        for channel in range(1,self.psu.channels+1):
+            configured_voltage = [x['voltage'] for x in TEST_KORAD_CONFIG['voltage'] if x['ch'] == channel][0]
+            self.assertAlmostEqual(self.psu.configured_voltage(channel), configured_voltage, delta=0.001, msg='Set voltage mismatch')
+    
+    def test_configured_current(self):
+        for channel in range(1,self.psu.channels+1):
+            configured_current = [x['current'] for x in TEST_KORAD_CONFIG['current'] if x['ch'] == channel][0]
+            self.assertAlmostEqual(self.psu.configured_current(channel), configured_current, delta=0.0005, msg='Set current mismatch')
+
+    def test_ocp(self):
+        enable = True
+        self.psu.set_ocp(enable)
+        self.assertEqual(self.psu.status['ocp'],enable,'OCP status not recognized correctly')
+
+    def test_output(self):
+        self.psu.set_output(False)
+        status = self.psu.status
+        self.assertEqual(status['output'],False,'output status not recognized correctly')
+        self.assertEqual(status['v_out'],0,'ouput may not be disabled')
+        self.assertEqual(status['i_out'],0,'ouput may not be disabled')
+        self.psu.set_output(True)
+
+
+if __name__ == "__main__":
+    unittest.main()
